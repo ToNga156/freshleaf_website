ec91934300a3cb39aa13cf004cb50a5c73b849fa
ThiKim148 <hothikim14102004@gmail.com>
2024-12-11 14:59:35 +0700

    update code hidden and display password

--- app/views/register.php
+++ app/views/register.php
@@ -6,13 +6,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
         body {
             display: flex;
             justify-content: center;
             align-items: center;
             font-family: Arial, sans-serif;
-            background-image: url(/app/views/image/background_regis_login.jpg);
+            background-image: url(../views/image/background_regis_login.jpg);
             background-size: cover;
             background-position: center center; 
         
@@ -25,7 +26,7 @@
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             width: 500px;
             text-align: center;
-            margin-top: 80px;
+            margin-top: 100px;
         }
         .container_register h2 {
             margin-bottom: 20px;
@@ -40,7 +41,7 @@
             margin: 10px;
             margin-left: 10px;
             border: 1px solid #ccc;
-            border-radius: 5px;
+            border-radius: 5px;   
             font-size: 18px;
         }
         .container_register .checkbox_register {
@@ -83,12 +84,15 @@
         .input_register {
             position: relative;
         }
-        .input_register .fa-eye {
-            position: absolute;
-            right: 10px;
-            top: 50%;
-            transform: translateY(-50%);
-            cursor: pointer;
+        .input_register .bi-eye-slash, 
+        .input_register .bi-eye { 
+            position: absolute; 
+            right: 20px; top: 50%; 
+            transform: translateY(-50%); 
+            cursor: pointer; 
+        } 
+        .input_register .hidden { 
+            display: none; 
         }
     </style>
 </head>
@@ -107,11 +111,13 @@
             </div>
             <div class="input_register">
                 <input type="password" name="password" placeholder="Password" required>
-                <i class="fas fa-eye"></i>
+                <i class="bi bi-eye-slash" id="close-password"></i> 
+                <i class="bi bi-eye hidden" id="open-password"></i>
             </div>
             <div class="input_register">
                 <input type="password" name="confirm_password" placeholder="Confirm Password" required>
-                <i class="fas fa-eye"></i>
+                <i class="bi bi-eye-slash" id="close-confirmpassword"></i> 
+                <i class="bi bi-eye hidden" id="open-confirmpassword"></i>
             </div>
             <button class="btt_signin">Sign In</button>
             <div class="footer_register">
@@ -119,5 +125,35 @@
             </div>
         </form>
     </div>
+    <script>
+        const closePassword = document.getElementById('close-password'); 
+        const openPassword = document.getElementById('open-password'); 
+        const passwordInput = document.querySelector('input[name="password"]'); 
+
+        closePassword.addEventListener('click', function() { 
+            passwordInput.setAttribute('type', 'text'); 
+            closePassword.classList.add('hidden'); 
+            openPassword.classList.remove('hidden'); 
+        }); 
+        openPassword.addEventListener('click', function() { 
+            passwordInput.setAttribute('type', 'password'); 
+            closePassword.classList.remove('hidden'); 
+            openPassword.classList.add('hidden'); 
+        }); 
+        const closeConfirmPassword = document.getElementById('close-confirmpassword'); 
+        const openConfirmPassword = document.getElementById('open-confirmpassword'); 
+        const confirmPasswordInput = document.querySelector('input[name="confirm_password"]'); 
+        
+        closeConfirmPassword.addEventListener('click', function() { 
+            confirmPasswordInput.setAttribute('type', 'text'); 
+            closeConfirmPassword.classList.add('hidden'); 
+            openConfirmPassword.classList.remove('hidden'); 
+        }); 
+        openConfirmPassword.addEventListener('click', function() { 
+            confirmPasswordInput.setAttribute('type', 'password'); 
+            closeConfirmPassword.classList.remove('hidden'); 
+            openConfirmPassword.classList.add('hidden'); 
+        }); 
+    </script>
 </body>
 </html>
