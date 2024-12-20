ec01e6dfa0116e5b1c59223aec9ddd06b9557c9d
nguyendat030805 <dat656473@gmail.com>
2024-12-20 12:13:20 +0700

    done views shopping cart

--- mvc/views/ShoppingCart.php
+++ mvc/views/ShoppingCart.php
@@ -61,7 +61,7 @@
                                 <button class="quantity-btn plus">+</button>
                             </div>
                         </td>
-                        <td>$14.00</td>
+                        <td class="subtotal">$14.00</td>
                     </tr>
                 </tbody>
             </table>
@@ -85,5 +85,6 @@
 </html>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="../../Public/js/shoppingCart.js"></script>
 </body>
 </html>
