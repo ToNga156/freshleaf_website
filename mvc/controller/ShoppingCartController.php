1986fec2c8aa99ef1869b950dfed945559752e1d
ToNga156 <ngaichi2004@gmail.com>
2024-12-27 14:16:35 +0700

    finish addtoCart

--- mvc/controller/ShoppingCartController.php
+++ mvc/controller/ShoppingCartController.php
@@ -13,7 +13,7 @@ class ShoppingCartController extends Controller {
             return;
         }
     
-        $user_id = $_SESSION['user_id']; // Lấy user_id từ session
+        $user_id = $_SESSION['user_id'];
         $data = json_decode(file_get_contents("php://input"), true);
         $product_id = $data['product_id'];
     
@@ -21,25 +21,20 @@ class ShoppingCartController extends Controller {
         $orderDetailModel = new OrderDetailModel();
         $productModel = new ProductModel();
     
-        // Kiểm tra nếu đã có đơn hàng pending
         $order_id = $orderModel->getPendingOrderId($user_id);
         if (!$order_id) {
             $order_id = $orderModel->createOrder($user_id);
         }
 
-        // Lấy giá sản phẩm
         $price = $productModel->getProductPrice($product_id);
     
-        // Kiểm tra sản phẩm trong bảng order_detail
         $existingProduct = $orderDetailModel->getOrderDetail($order_id, $product_id);
     
         if ($existingProduct) {
-            // Nếu sản phẩm đã tồn tại, tăng số lượng lên 1
             $newQuantity = $existingProduct['quantity'] + 1;
             $orderDetailModel->updateQuantity($order_id, $product_id, $newQuantity);
             echo json_encode(["success" => true, "message" => "Đã cập nhật số lượng sản phẩm."]);
         } else {
-            // Nếu sản phẩm chưa tồn tại, thêm mới sản phẩm vào order_detail
             $orderDetailModel->addProductToOrder($order_id, $product_id, 1, $price);
             echo json_encode(["success" => true, "message" => "Đã thêm sản phẩm mới vào giỏ hàng."]);
         }
@@ -59,14 +54,14 @@ class ShoppingCartController extends Controller {
         $orderModel = new OrderModel();
         $orderDetailModel = new OrderDetailModel();
 
-        // Lấy đơn hàng pending
         $order_id = $orderModel->getPendingOrderId($user_id);
         $cartItems = [];
+        $totalQuantity = [];
         if ($order_id) {
-            // Lấy sản phẩm trong giỏ hàng
             $cartItems = $orderDetailModel->getOrderDetails($order_id);
+            $totalQuantity = $orderDetailModel->getTotalQuantityCart($order_id);
         }
 
-        $this->view("ShoppingCart", ["cartItems" => $cartItems]);
+        $this->view("ShoppingCart", ["cartItems" => $cartItems, "totalQuantity" => $totalQuantity]);
     }
 }
